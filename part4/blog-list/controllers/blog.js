6c609b11a1c199ba9d0a0d5f8055cb55a691e816
hp <nikhilrk13jan@gmail.com>
2021-10-19 21:03:14 +0530

    excercises 4.13, 4.14 done

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const mongoose = require('mongoose')
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async(request, response, next) => {
@@ -17,6 +18,26 @@ blogRouter.post('/',async (request, response, next) => {
     response.status(400).end()
   }
 
+})
+
+blogRouter.delete('/:id', async (request, response, next) => {
+  const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
+  if(deletedBlog){
+    response.status(200).json(deletedBlog)
+  }
+  else{
+    response.status(400).end()
+  }
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body)
+  if(updatedBlog){
+    response.status(200).json(updatedBlog)
+  }
+  else{
+    response.status(400).end()
+  }
 })
 
 module.exports = blogRouter  
--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -7,70 +7,71 @@ const Blog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let newObject = new Blog(helper.initialBlogs[0])
-  await newObject.save()
-  newObject = new Blog(helper.initialBlogs[1])
-  await newObject.save()
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('when some initial blogs are saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all posts are returned',async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-}, 100000)
+  test('all posts are returned',async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-test('an id is defined', async () => {
-  let response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type',/application\/json/)
-  expect(response.body[0].id).toBeDefined()  
-  expect(response.body[0]._id).toBe(undefined)
-  
-})
-
-test('a valid blog entry can be added', async () => {
-  const newBlog = {
-    title:'duckduckgo, search here',
-    author:'duckduckgo',
-    url:'https://www.duckduckgo.com',
-    likes:'7'
-  }
-  
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type',/application\/json/)
-  
-  const blogsAtEnd = await helper.blogsInDb() 
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain('duckduckgo, search here') 
-  
-})
-
-test('likes must have default value 0', async () => {
-  const newBlog = {
-    title:'blog with no likes',
-    author:'anonymous',
-    url:'anyxyz.com',
-  }
-
-  const response = await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type',/application\/json/)
+  test('an id is defined in returned response', async () => {
+    let response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type',/application\/json/)
+    expect(response.body[0].id).toBeDefined()  
+    expect(response.body[0]._id).toBe(undefined)
     
-  expect(response.body.likes).toBe(0)
+  })
+
+  test('likes must have default value 0', async () => {
+    const newBlog = {
+      title:'blog with no likes',
+      author:'anonymous',
+      url:'anyxyz.com',
+    }
+  
+    const response = await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type',/application\/json/)
+      
+    expect(response.body.likes).toBe(0)
+  })
+})
+
+describe('adding a new blog entry', () => {
+  test('a valid blog entry can be added', async () => {
+    const newBlog = {
+      title:'duckduckgo, search here',
+      author:'duckduckgo',
+      url:'https://www.duckduckgo.com',
+      likes:'7'
+    }
+    
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type',/application\/json/)
+    
+    const blogsAtEnd = await helper.blogsInDb() 
+  
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('duckduckgo, search here') 
+    
+  })
 })
 
 describe('title and url are required fields',() => {
@@ -97,6 +98,42 @@ describe('title and url are required fields',() => {
     await api.post('/api/blogs')
       .send(noUrlBlog)
       .expect(400) 
+  })
+})
+
+describe('deletion of a blog', () => {
+  test('success with 200 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+    const blogTitles = blogsAtEnd.map(blog => blog.title)
+    expect(blogTitles).not.toContain(blogToDelete.title)
+  })
+})
+
+describe('updation a blog entry', () => {
+  test('updating title of an existing blog',async () => {
+    const blogsAtStart = await helper.blogsInDb()
+
+    const blogToBeUpdated = blogsAtStart[0]
+
+    blogToBeUpdated.title = 'this title is updated'
+
+    await api
+      .put(`/api/blogs/${blogToBeUpdated.id}`)
+      .send(blogToBeUpdated)
+      .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain(blogToBeUpdated.title)
   })
 })
 
